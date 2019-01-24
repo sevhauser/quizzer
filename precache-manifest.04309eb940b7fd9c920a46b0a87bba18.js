cef373edd3251a3bf507dd8d7831f7a06033d820
Severin <mynameiswowbagger@gmail.com>
2019-01-24 16:50:50 +0100

    First Deploy

---
+++ precache-manifest.04309eb940b7fd9c920a46b0a87bba18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/quizzer/robots.txt"
+  },
+  {
+    "revision": "d757eae68e564b881549",
+    "url": "/quizzer/js/chunk-vendors.13ed1a72.js"
+  },
+  {
+    "revision": "e38088c1d733b6f6a1e1",
+    "url": "/quizzer/js/app.863d7075.js"
+  },
+  {
+    "revision": "f7c2f7dff47c0c4a743c98e640b9f5af",
+    "url": "/quizzer/index.html"
+  },
+  {
+    "revision": "d757eae68e564b881549",
+    "url": "/quizzer/css/chunk-vendors.0f353578.css"
+  },
+  {
+    "revision": "e38088c1d733b6f6a1e1",
+    "url": "/quizzer/css/app.1fbf6f59.css"
+  }
+];
